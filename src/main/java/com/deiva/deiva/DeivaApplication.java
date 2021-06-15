db78eb597092a613f68fb5616fb47b1cbc0888e2
Luis Fernando <lmartinez.bno@gmail.com>
2021-06-15 09:04:27 -0600

    first commit

---
+++ src/main/java/com/deiva/deiva/DeivaApplication.java
@@ -0,0 +1,13 @@
+package com.deiva.deiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeivaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deiva/deiva/DeivaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deiva.deiva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
